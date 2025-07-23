d83952b0a3c52d774b492d1d723cd049026ade73
= <=>
2025-07-23 21:53:35 +0530

    Number Methods

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,38 @@
+const score = 400;
+// console.log(score);
+
+const balance = new Number(100)
+// console.log(balance)
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(3));
+
+const otherNumber = 123.8996
+
+// console.log(otherNumber.toPrecision(4));
+
+const hundreds = 100000
+// console.log(hundreds.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++ Maths ++++++++++++++++++++++++++++++
+
+console.log(Math);
+console.log(Math.abs(-4));
+console.log(Math.round(5,6));
+console.log(Math.ceil(6,3));
+console.log(Math.floor(9,7));
+console.log(Math.min(2,4,3,6,7,9));
+console.log(Math.max(2,4,3,6,7,9));
+
+
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
+
