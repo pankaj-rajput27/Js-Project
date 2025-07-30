e6781fdfb8d91db30310efe264a1d73e8e101788
= <=>
2025-07-30 14:54:36 +0530

    IIFE Function

---
+++ 03_basics/04_iife.js
@@ -0,0 +1,14 @@
+// Immediately Invoked Function Expressions (IIFE)
+
+
+// named IIFE Function
+(function one(){
+    console.log('Welcome to The Immediately Invoked Function Expressions (IIFE)');
+})();
+
+
+// Simple IIFE Function
+((funName)=>{
+    console.log(`Welcome to The ${funName}`);
+    
+})(' Immediately Invoked Function Expressions (IIFE)')
