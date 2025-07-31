f426c65e4107709bce92bd98fe099ed090165f9d
= <=>
2025-08-01 00:03:56 +0530

    while & Do while Loops

---
+++ 05_iterations/two.js
@@ -0,0 +1,28 @@
+// while Loops
+
+let i = 1
+// while (i <=10) {
+//     console.log( `Number is ${i}` );
+//     i++
+// }
+
+// let myArray = ['Rohit', 'Virat', 'sky', 'dhoni']
+
+// let arr = 0
+// while (arr < myArray.length) {
+//     console.log(`Valuse is ${myArray[arr]}`);
+//     arr++
+// }
+
+// ---------------------------------------------------------
+
+// Do while Loops
+
+// let score = 1
+let score = 12
+
+do {
+    console.log(`Score is ${score}`);
+    score++
+    
+} while (score <=10);
